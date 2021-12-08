b81590166bbb70a074b72ee5b6c205f6ea7f723c
Mohammad Wildan Rizkiyadi <outsiderkemuning@gmail.com>
2021-12-08 09:14:12 +0700

    Fix Flag Country

--- src/components/getMyCity.vue
+++ src/components/getMyCity.vue
@@ -3,7 +3,7 @@
         <span v-for="(key, d) in country" :key="d">
             <span v-if="d == info_country"> {{ key }} </span>
         </span>
-        <img :src="'https://flagcdn.com/'+info_country+'.svg'" :style="'height: 24px; margin-left: 5px'">
+        <img :src="'https://flagcdn.com/24x18/'+info_country+'.png'" :style="'margin-left: 5px'">
     </h2>
 </template>
 
